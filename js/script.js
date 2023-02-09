8a6f0962e6cebe0b102c6b4e3879d3ba707d0577
jeel-simform <jeel.v@simformsolutions.com>
2023-02-09 17:23:28 +0530

    completed single page application

---
+++ js/script.js
@@ -0,0 +1,29 @@
+let slideIndex = 1;
+
+const showImage=(n)=> {
+    let slide = document.getElementsByClassName('slides'), 
+        dots = document.getElementsByClassName('dots'),
+        i;
+    if (n > slide.length) { 
+        slideIndex = 1;
+    }
+    if (n < 1) {    
+        slideIndex = slide.length;
+    }
+    for (i = 0; i < slide.length; i++) { 
+        slide[i].style.display = 'none';
+    }
+    slide[slideIndex - 1].style.display = 'block';
+    for (i = 0; i < dots.length; i++) {   
+        dots[i].className = dots[i].className.replace(' active', '');
+    }
+    dots[slideIndex - 1].className += ' active';
+}
+showImage(slideIndex);
+
+const plusIndex=(n)=> {  
+    showImage(slideIndex += n);
+}
+const currentSlide=(n)=> { 
+    showImage(slideIndex = n);
+}
---
+++ js/scroll.js
@@ -0,0 +1,13 @@
+const about_ele=document.querySelector('.main-about')
+const image_ele=document.querySelector('.slider-container')
+const service_ele=document.querySelector('.main-service')
+
+const about=()=>{
+    window.scroll(0,about_ele.offsetTop)
+}
+const image=()=>{
+    window.scroll(0,image_ele.offsetTop)
+}
+const service=()=>{
+    window.scroll(0,service_ele.offsetTop)
+}
