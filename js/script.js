c960d5f52372f334e7d2b82b45ffcce12d210d52
jeel-simform <jeel.v@simformsolutions.com>
2023-02-10 10:36:12 +0530

    changed app.js

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ const showImage=(n)=> {
     let slide = document.getElementsByClassName('slides'), 
         dots = document.getElementsByClassName('dots'),
         i;
+
         slideIndex=n>slide.length?1: n<1?slide.length:n;
     for (i = 0; i < slide.length; i++) { 
         slide[i].style.display = 'none';
